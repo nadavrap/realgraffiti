d4eccb0f2d160ad9d83610b8bc322f0a3934e81f
alexpoo <alexpoo@localhost>
2011-06-17 23:24:29 +0300

    Added RestClient

--- src/realgraffiti/android/data/RealGraffitiDataProxy.java
+++ src/realgraffiti/android/data/RealGraffitiDataProxy.java
@@ -1,6 +1,18 @@
 package realgraffiti.android.data;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import realgraffiti.common.data.RealGraffitiData;
 import realgraffiti.common.dto.GraffitiDto;
@@ -9,13 +21,13 @@ import realgraffiti.common.dto.GraffitiLocationParametersDto;
 public class RealGraffitiDataProxy implements RealGraffitiData{
 
 	@Override
-	public void addNewGraffitiDto(GraffitiDto GraffitiDto) {
-		// TODO Auto-generated method stub
-		
+	public void addNewGraffiti(GraffitiDto GraffitiDto) {
+			
+
 	}
 
 	@Override
-	public Collection<GraffitiDto> getNearByGraffitiDto(
+	public Collection<GraffitiDto> getNearByGraffiti(
 			GraffitiLocationParametersDto graffitiLocationParameters) {
 		// TODO Auto-generated method stub
 		return null;
