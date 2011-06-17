0e6e2c792f7bf9d57f6d6a5694c72ecc533bde8c
alexpoo <alexpoo@localhost>
2011-06-17 20:58:09 +0300

    Added data proxy

---
+++ src/realgraffiti/android/data/RealGraffitiDataProxy.java
@@ -0,0 +1,24 @@
+package realgraffiti.android.data;
+
+import java.util.Collection;
+
+import realgraffiti.common.data.RealGraffitiData;
+import realgraffiti.common.dto.GraffitiDto;
+import realgraffiti.common.dto.GraffitiLocationParametersDto;
+
+public class RealGraffitiDataProxy implements RealGraffitiData{
+
+	@Override
+	public void addNewGraffitiDto(GraffitiDto GraffitiDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<GraffitiDto> getNearByGraffitiDto(
+			GraffitiLocationParametersDto graffitiLocationParameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
